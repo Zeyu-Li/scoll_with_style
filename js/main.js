2f4ad94b0e8feb26039ad1554431c71b7c430f10
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-12-03 23:48:34 -0700

    added lib and fixed js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 
+// Variable number of scrollbar options
 $(document).ready(function () {
-    let options = ['No scrollbar', 'Regular', 'Mac', 'Thicker Mac', 'Dark', 'Light', 'Ubuntu', 'Minty', 'Colored', 'Rainbow' ]
+    let options = ['No scrollbar', 'Regular', 'Mac', 'Thicker Mac', 'Dark', 'Light', 'Ubuntu', 'Minty', 'Colored', 'Rainbow'];
     for (i=0; i<options.length; i++) {
-        $('.options').append('<div class="flex-container"><div class="form-check form-check-inline"><input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio'+ i +'" value="option'+ i +'" onchange="change()"><label class="form-check-label" for="inlineRadio'+ i +'">'+ options[i] +'</label> </input></div><br></div>')
+        $('.options').append('<div class="flex-container"><div class="form-check form-check-inline"><input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio'+ i +'" value="option'+ i +'"><label class="form-check-label" for="inlineRadio'+ i +'">'+ options[i] +'</label> </input></div><br></div>');
     }
-})
+
+
+    // Highlights selected
+    var old_name = 0;
+    $('.form-check-input').click(function () {
+        if (old_name !== 0) {
+            $(old_name).parent().css({'border': 'none', 'background-color': 'white'});
+        }
+        var name = $("input[name='inlineRadioOptions']:checked");
+
+        $(name).parent().css({'border': '1px solid black','background-color': 'rgb(224, 224, 224)'});
+        old_name = name;
+
+        console.log(name, old_name);
+
+    });
+});
+
+// Hid scollbar after certain time period
 $('#hid').click( function() {
     if ($('#hid').is(':checked')) {
         $('.modal-content').css('background-color', 'white');
@@ -12,15 +31,3 @@ $('#hid').click( function() {
         $('.modal-content').css('background-color', 'rgb(224, 224, 224)');
     }
 });
-function change() {
-    for (i=0; i<$('.options input').length; i++) {
-        let name = '#inlineRadio'+i;
-        if ($(name).is(':checked')) {
-            $(name).parent().css({'border': '1px solid black',
-                                    'background-color': 'rgb(224, 224, 224)'});
-        } else {
-            $(name).parent().css({'border': 'none',
-                                    'background-color': 'white'});
-        }
-    }
-};
